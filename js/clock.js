8fa20cc607b51e271ebccb4fac038fb9887223be
Orcablast <59005472+Orcablast@users.noreply.github.com>
2020-01-28 13:15:21 +0900

    clock update 0128
    fix hours over 10:00

--- js/clock.js
+++ js/clock.js
@@ -7,8 +7,7 @@ function getTime() {
   const hours = date.getHours();
   const seconds = date.getSeconds();
   clockTitle.innerText = `${hours < 12 ? `오전` : "오후"} ${
-    hours < 10 ? `0${hours}` : `0${hours - 12}`
-  }:${minutes < 10 ? `0${minutes}` : minutes}:${
+    hours < 10 ? `0${hours}` : `${hours > 12 ? `${hours - 12}` : `${hours}` }`}:${minutes < 10 ? `0${minutes}` : minutes}:${
     seconds < 10 ? `0${seconds}` : seconds
   }`;
 }
